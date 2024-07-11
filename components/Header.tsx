52d1d95d477031ac712303fe91ddd3921ec2a1bc
manlikexavy <chibokaxavier@gmail.com>
2024-07-11 19:11:28 +0100

    header, mobile nav and nav completed

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+import Nav from "@/components/Nav";
+import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 ">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href={"/"}>
+          <h1 className="text-4xl font-semibold">
+            BlooCodeTech<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        <div className="hidden xl:flex gap-8 justify-center items-center">
+          <Nav />
+        </div>
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
