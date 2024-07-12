dde275a2485f52e40c3229e4384ef685b1d44022
manlikexavy <chibokaxavier@gmail.com>
2024-07-12 12:35:30 +0100

    dynamic page for beauty completed

--- app/beauty/[id]/page.tsx
+++ app/beauty/[id]/page.tsx
@@ -13,14 +13,15 @@ interface Product {
   // Add other properties as per your actual API response
 }
 
-export default function Home() {
+export default function Home({ params }: { params: { id: String } }) {
   const [products, setProducts] = useState<Product[]>([]);
-  const [beautyProducts, setBeautyProducts] = useState<Product[]>([]);
+  const [beauty, setBeauty] = useState<Product | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   useEffect(() => {
     const fetchProducts = async () => {
+      setLoading(true);
       try {
         const res = await fetch("https://dummyjson.com/products");
         const result = await res.json();
@@ -35,37 +36,29 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
+    console.log("params.id:", params.id); // Debugging statement
+    console.log("products:", products); // Debugging statement
     if (products.length > 0) {
-      const beauty = products.filter((beauty) => beauty.category === "beauty");
-      setBeautyProducts(beauty);
+      const beautyProduct = products.find((item) => item.id.toLocaleString() === params.id);
+      console.log("beautyProduct:", beautyProduct)
+      setBeauty(beautyProduct || null);
+      console.log(beautyProduct);
     }
-  }, [products]);
+  }, [products, params.id]);
 
   return (
     <main className=" container">
-      {/* <div className="">
-        <Ads />
-      </div> */}
       <div className="flex  flex-col gap-[100px] justify-center items-center mb-20">
-        {/* <p className="text-4xl font-semibold">Beauty</p> */}
         <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3 gap-4 ">
-          {beautyProducts.map((product: Product) => (
-            <div key={product.id} className="">
-              <Link href={`/beauty/${product.id}`}>
-                <li className=" bg-gray-200 rounded-lg">
-                  <img src={product.thumbnail} alt={product.title} />
-                </li>
-                <div className="flex justify-between gap-5 pt-5">
-                  <h2 className="text-xl font-bold leading-6">
-                    {product.title}
-                  </h2>{" "}
-                  <p className="text-gray-500 font-light">
-                    Price:${product.price}
-                  </p>
-                </div>
-              </Link>
+          <div key={beauty?.id} className="">
+            <li className=" bg-gray-200 rounded-lg">
+              <img src={beauty?.thumbnail} alt={beauty?.title} />
+            </li>
+            <div className="flex justify-between gap-5 pt-5">
+              <h2 className="text-xl font-bold leading-6">{beauty?.title}</h2>{" "}
+              <p className="text-gray-500 font-light">Price:${beauty?.price}</p>
             </div>
-          ))}
+          </div>
         </ul>
       </div>
     </main>
